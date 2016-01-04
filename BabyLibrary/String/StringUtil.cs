15f816b4c58ac08fcc92df77e598e308dd8d7546
Babybaby0 <378344765@qq.com>
2016-01-04 20:04:02 +0800

    Test
    Test

--- BabyLibrary/String/StringUtil.cs
+++ BabyLibrary/String/StringUtil.cs
@@ -4,6 +4,9 @@ namespace StringUtil
 {
     public class HandleString
     {
-        public static 
+        public static bool StringCompare(string str1, string str2)
+        {
+            return string.Equals(str1, str2);
+        }
     }
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+#.Net 学习笔记
+[TOC]
+## string
+### string.Equal()和"=="
+1."==" 是一个运算符。
+2.Equals则是string对象的方法，可以.（点）出来。
+### 
